5641655be76b97fa682e93ff11511f441ddf366f
Baptiste Firket <baptistefirket@gmail.com>
2019-03-13 21:14:13 +0100

    added styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Header from './header'
+import Footer from './footer'
 import './layout.css'
+
+const AppContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:300,400');
+  font-family: 'Nunito Sans', sans-serif;
+
+  h1 {
+    font-family: 'Nunito Sans';
+  }
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,24 +35,11 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <AppContainer>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+        <main>{children}</main>
+        <Footer />
+      </AppContainer>
     )}
   />
 )
