1cfacbbb457dd814f9188c8b900a247d80be2647
Baptiste Firket <baptistefirket@gmail.com>
2019-03-20 09:04:40 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,15 +4,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
-  background-color: #eee;
-  height: 200px;
+  background-color: #282828;
+  color: #aaa;
+  text-align: center;
+  min-height: 50px;
+  padding: 1rem 0;
+  font-size: 0.8rem;
+
+  a {
+    text-decoration: none;
+    color: #aaa;
+
+    &:hover {
+      color: #ddd;
+    }
+  }
 `
 
 const Footer = () => (
   <StyledFooter>
-    © {new Date().getFullYear()}, Built with
+    © {new Date().getFullYear()}, all rights reserved. This website is designed
+    and developed by me with
     {` `}
     <a href="https://www.gatsbyjs.org">Gatsby</a>
+    {` `}❤
   </StyledFooter>
 )
 
