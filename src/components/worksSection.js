ae563f11ac42c4c361da0bc28fa48ee6b28195f0
Baptiste Firket <baptistefirket@gmail.com>
2019-03-28 22:20:37 +0100

    Update worksSection.js

--- src/components/worksSection.js
+++ src/components/worksSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import styled from 'styled-components'
 
-import ProjectCard from './ProjectCard'
+import ProjectCard from './projectCard'
 
 const WorkContainer = styled.div`
   width: 90%;
