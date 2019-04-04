60d3ac7471d42aa3c8d23fd4a01b55b3ee2f99a3
Baptiste Firket <baptistefirket@gmail.com>
2019-04-04 17:58:27 +0200

    Update works.js

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import WorksSection from '../components/worksSection'
 
 const PageWrapper = styled.div`
-  padding: 130px 0;
+  padding: 110px 0;
 
   h1 {
     margin-left: 7%;
@@ -18,14 +19,42 @@ const PageWrapper = styled.div`
   }
 `
 
-const Work = () => (
+const Work = ({ data }) => (
   <Layout>
     <SEO title="Work" />
     <PageWrapper>
       <h1>My work</h1>
-      <WorksSection />
+      <WorksSection data={data} />
     </PageWrapper>
   </Layout>
 )
 
 export default Work
+
+export const query = graphql`
+  query allProjectQuery {
+    allContentfulProject {
+      edges {
+        node {
+          title
+          summary {
+            summary
+          }
+          slug
+          screenshot {
+            title
+            fluid(maxWidth: 500) {
+              ...GatsbyContentfulFluid
+            }
+          }
+          year
+          type
+          role
+          techno
+          collab
+          link
+        }
+      }
+    }
+  }
+`
