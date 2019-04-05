c4bf93e3cdfb33089a3ed07c721723ac5b8e8adc
Baptiste Firket <baptistefirket@gmail.com>
2019-04-05 13:10:53 +0200

    Update projectCard.js

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -23,15 +23,33 @@ const ProjectContainer = styled.div`
   } */
 
   h3 {
-    font-size: 1.4rem;
+    font-size: 1.6rem;
     margin-bottom: 0;
-    color: #222;
+    color: #437c90;
   }
 
   p {
-    color: #888;
-    margin-top: 1rem;
-    font-size: 0.9rem;
+    color: #437c90;
+    margin-top: 0.5rem;
+    margin-bottom: 0;
+    font-size: 16px;
+    /* font-weight: 700; */
+
+    /* span {
+      font-size: 12px;
+      font-weight: 900;
+      color: #437c90;
+      margin-left: 10px;
+    } */
+  }
+
+  .role {
+    color: #7eaab7;
+    margin-right: 8px;
+    font-size: 0.75rem;
+    /* padding: 0 8px; */
+    /* background-color: #ffca96;
+    border-radius: 25px; */
   }
 `
 const ImgWrapper = styled.div`
@@ -40,7 +58,7 @@ const ImgWrapper = styled.div`
   margin: 1.5rem auto;
   box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
     0 8px 16px -8px rgba(0, 0, 0, 0.3);
-  transition: 0.2s ease-in;
+  transition: 0.1s ease-in;
 
   &:hover {
     box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
@@ -55,12 +73,12 @@ const TitleAndTag = styled.div`
   align-items: center;
 `
 const Tag = styled.div`
-  background-color: #eee;
-  color: #999;
+  background-color: #7eaab7;
+  color: #fff8ef;
   font-size: 0.7rem;
   width: fit-content;
-  padding: 2px 5px;
-  border-radius: 4px;
+  padding: 1px 6px;
+  border-radius: 25px;
   font-weight: 700;
 `
 
@@ -83,7 +101,13 @@ const ProjectCard = ({
         <span>{techno[0]}</span>
       </Tag>
     </TitleAndTag>
-    <p>{`${type[0]} | ${role}`}</p>
+    <p>
+      {type[0]}
+      {/* <span>{year.substring(0, 4)}</span> */}
+    </p>
+    {role.map(item => (
+      <span className="role" key={item}>{`| ${item}`}</span>
+    ))}
     <Link to={`/work/${slug}`}>
       <ImgWrapper>
         <Img fluid={img[0].fluid} alt={img[0].title} />
